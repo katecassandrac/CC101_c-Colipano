4fd3fa8a9e89dd5aed95a004942361bf33976eb3
Kate Cassandra E. Colipano <katecassandracolipano8@gmail.com>
2024-11-22 10:43:52 +0800

    Add files via upload

--- KateCassandraColipano03v2.cpp
+++ KateCassandraColipano03v2.cpp
@@ -7,6 +7,7 @@ int main()
 
 	int age, time;
 	char user, m2;
+	float money;
 	
 	cout << "Program that you can attend to." << endl;
 	cout << "Enter your age: ";
@@ -23,7 +24,7 @@ int main()
 			cout << "You can watch the rated G movies.";
 			}
 	else if ( age == 13 && age < 16 ){
-		cout << "Are you going to watch the movie with your parents [1: Yes, 2: No]: ";
+		cout << "Are you going to watch the movie with your parents [1: Yes, 2: No]: " << endl;
 	}
 			if (m2 == 1){
 				cout <<"You can watch the rated G, PG, R movies.";
@@ -32,15 +33,27 @@ int main()
 				cout << "You can watch the rated G, PG movies.";
 			}
 	else if (age > 16) {
-		cout  << "You can watch the rated G, PG, R movies. ";
+		cout  << "You can watch the rated G, PG, R movies. " << endl;
 	}
 	cout << "Night or Morning? [1=Night, 2=Morning.]";
 	cin >> time;
 	if( time == '1'){
 		cout << "Evening:$10.50 ";
 		}
+		 cout << "Enter Amount of Money: ";
+		 cin >> money;
+		 
+			if ( money < 10.50) {
+				cout << "Not enough money." ;
+			}
+			
 		else if (time == '2'){
 		cout << "Matinee:$7.50";
 		}
+			cout << "Enter Amount of Money: ";
+			cin >> money;
+				 if ( money < 10.50) {
+				cout << "Not enough money." ;
+				}
 	return 0;
 } 
